91e1f25ce307fb06e86a694c87b2518d6387c9a2
Patrick Barber <patbarber72@gmail.com>
2022-06-16 08:14:39 +1000

    added logo svg to header

--- platform/ui/src/components/Header/Header.jsx
+++ platform/ui/src/components/Header/Header.jsx
@@ -4,33 +4,60 @@ import { useTranslation } from 'react-i18next';
 import classNames from 'classnames';
 import { NavBar, Svg, Icon, IconButton, Dropdown } from '../';
 
-function Header({ children, menuOptions, isReturnEnabled, onClickReturnButton, isSticky, WhiteLabeling }) {
+function Header({
+  children,
+  menuOptions,
+  isReturnEnabled,
+  onClickReturnButton,
+  isSticky,
+  WhiteLabeling,
+}) {
   const { t } = useTranslation('Header');
-
+  //test
   // TODO: this should be passed in as a prop instead and the react-router-dom
   // dependency should be dropped
   const onClickReturn = () => {
     if (isReturnEnabled && onClickReturnButton) {
-      onClickReturnButton()
+      onClickReturnButton();
     }
   };
 
-  const CustomLogo = (React) => {
-    return WhiteLabeling.createLogoComponentFn(React)
-  }
+  const CustomLogo = React => {
+    return WhiteLabeling.createLogoComponentFn(React);
+  };
 
   return (
-    <NavBar className='justify-between border-b-4 border-black' isSticky={isSticky}>
+    <NavBar
+      className="justify-between border-b-4 border-black"
+      isSticky={isSticky}
+    >
       <div className="flex justify-between flex-1">
         <div className="flex items-center">
           {/* // TODO: Should preserve filter/sort
               // Either injected service? Or context (like react router's `useLocation`?) */}
           <div
-            className={classNames("inline-flex items-center mr-3", isReturnEnabled && 'cursor-pointer')}
+            className={classNames(
+              'inline-flex items-center mr-3',
+              isReturnEnabled && 'cursor-pointer'
+            )}
             onClick={onClickReturn}
           >
-            {isReturnEnabled && <Icon name="chevron-left" className="w-8 text-primary-active" />}
-            <div className="ml-4">{WhiteLabeling ? CustomLogo(React) : <Svg name="logo-ohif" />}</div>
+            {isReturnEnabled && (
+              <Icon name="chevron-left" className="w-8 text-primary-active" />
+            )}
+            <div className="ml-4">
+              {WhiteLabeling ? (
+                CustomLogo(React)
+              ) : (
+                <div className="w-fit">
+                  <Svg name="logo-aurabox" className="w-8 h-8 inline-block" />
+                  <p className="ml-4 text-white inline-block">
+                    {' '}
+                    Aurabox Viewer
+                  </p>
+                </div>
+              )}
+            </div>
           </div>
         </div>
         <div className="flex items-center">{children}</div>
@@ -40,7 +67,7 @@ function Header({ children, menuOptions, isReturnEnabled, onClickReturnButton, i
           </span>
           <Dropdown id="options" showDropdownIcon={false} list={menuOptions}>
             <IconButton
-              id={"options-settings-icon"}
+              id={'options-settings-icon'}
               variant="text"
               color="inherit"
               size="initial"
@@ -49,7 +76,7 @@ function Header({ children, menuOptions, isReturnEnabled, onClickReturnButton, i
               <Icon name="settings" />
             </IconButton>
             <IconButton
-              id={"options-chevron-down-icon"}
+              id={'options-chevron-down-icon'}
               variant="text"
               color="inherit"
               size="initial"
@@ -81,7 +108,7 @@ Header.propTypes = {
 
 Header.defaultProps = {
   isReturnEnabled: true,
-  isSticky: false
+  isSticky: false,
 };
 
 export default Header;
