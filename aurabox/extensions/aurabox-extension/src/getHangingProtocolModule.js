1797dac01ba760e4c8f77645c4717c4753fa7f98
Nigel Heap <nigel.c.heap@gmail.com>
2022-09-21 17:35:20 +1000

    mode and extension specificly for aurabox

---
+++ aurabox/extensions/aurabox-extension/src/getHangingProtocolModule.js
@@ -0,0 +1,81 @@
+export const defaultProtocol = {
+  id: 'default',
+  locked: true,
+  hasUpdatedPriorsInformation: false,
+  name: 'Default',
+  createdDate: '2022-09-21T19:22:08.894Z',
+  modifiedDate: '2022-09-21T19:22:08.894Z',
+  availableTo: {},
+  editableBy: {},
+  protocolMatchingRules: [],
+  toolGroupIds: ['default'],
+  stages: [
+    {
+      id: 'hYbmMy3b7pz7GLiaT',
+      name: 'default',
+      viewportStructure: {
+        type: 'grid',
+        properties: {
+          rows: 1,
+          columns: 1,
+        },
+      },
+      displaySets: [
+        {
+          id: 'displaySet',
+          // Unused currently
+          imageMatchingRules: [],
+          // Matches displaysets, NOT series
+          seriesMatchingRules: [
+            {
+              id: 'GPEYqFLv2dwzCM322',
+              weight: 4,
+              attribute: 'SeriesDescription',
+              constraint: {
+                doesNotEqual: {
+                  value: 'Report',
+                },
+              },
+              required: false,
+            },
+          ],
+          studyMatchingRules: [],
+        },
+      ],
+      viewports: [
+        {
+          viewportOptions: {
+            toolGroupId: 'default',
+            // initialImageOptions: {
+            //   index: 180,
+            //   preset: 'middle', // 'first', 'last', 'middle'
+            // },
+          },
+          displaySets: [
+            {
+              options: [],
+              id: 'displaySet',
+            },
+          ],
+        },
+      ],
+      createdDate: '2022-09-21T18:32:42.850Z',
+    },
+  ],
+  numberOfPriorsReferenced: -1,
+};
+
+const getHangingProtocolModule = ({
+  servicesManager,
+  commandsManager,
+  extensionManager,
+}) => {
+  return [
+    {
+      name: 'default',
+      protocols: [defaultProtocol],
+    },
+  ];
+};
+
+export default getHangingProtocolModule;
